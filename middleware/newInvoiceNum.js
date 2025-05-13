5cf4531d8bd50af925f41c8e284cfdf9119c86ce
Ketan Dudka <ketandudka566123@gmail.com>
2025-05-13 15:34:18 +0530

    push

--- middleware/newInvoiceNum.js
+++ middleware/newInvoiceNum.js
@@ -6,10 +6,10 @@ export const invoiceNumber = () => {
   let financialYear;
   if (currentMonth >= 4) {
     // If April or later, current year - next year
-    financialYear = `${currentYear}-${(currentYear + 1).toString().slice(-2)}`;
+    financialYear = `${currentYear}-${(currentYear + 1).toString()}`;
   } else {
     // If before April, previous year - current year
-    financialYear = `${currentYear - 1}-${currentYear.toString().slice(-2)}`;
+    financialYear = `${currentYear - 1}-${currentYear.toString()}`;
   }
 
   const prefix = `TIJ${financialYear}`;
