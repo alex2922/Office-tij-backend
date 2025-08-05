eec7a554e7b6340f61a8f06f7ef774bfa37322d2
Ketan Dudka <ketandudka566123@gmail.com>
2025-08-05 14:33:55 +0530

    push

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,17 @@ module.exports = {
   apps: [
     {
       name: "office-tij-backend",
-      script: "./index.js", // your main entry file
+      script: "./index.js",
       node_args: "--max-old-space-size=1024",
+      watch: false, // set to true only during dev
+      max_restarts: 5,
+      restart_delay: 5000, // wait 5s between restarts
       env: {
         NODE_ENV: "production"
-      }
+      },
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm Z"
     }
   ]
 };
